0c5bcf6d5b48b0b3fd13b98e9faa4d171ce1f464
saltyaom <saltyaom@gmail.com>
2023-06-05 00:23:01 +0700

    :tada: feat: cjs

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,12 @@
 import { Elysia } from 'elysia'
-import cors from '../src/index'
+import { cors } from '../src/index'
 
 const app = new Elysia()
-    .use(cors({
-        maxAge: 5
-    }))
-    .get('/', () => ({
-        hello: 'world'
-    }))
-    .post('/', () => 'Hi')
-    .listen(8080)
+    .use(cors())
+    .get('/', () => ({ same: 'a' }))
+    .listen(3000)
+
+console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
+
+export type App = typeof app
+console.log('Server is running on port 3000.')
